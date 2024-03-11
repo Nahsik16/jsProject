448d247e1c82dd5c857a7f88ae920e9e23989edf
Nahsik16 <118239409+Nahsik16@users.noreply.github.com>
2024-03-11 21:04:43 +0530

    first commit

---
+++ project-1/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    {/* <nav>
+      <div className="logo">
+        <img src="/images/brand_logo.png" alt="logo" />
+      </div>
+      <ul>
+        <li href="#">menu</li>
+        <li href="#">location</li>
+        <li href="#">about</li>
+        <li href="#">contact</li>
+      </ul>
+      <button className="login">login</button>
+    </nav> */}
+      
+      
+    </>
+  )
+}
+
+export default App
