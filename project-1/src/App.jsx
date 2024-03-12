72277fbcd71d74ce107a7499d641b06c25843d74
Nahsik16 <118239409+Nahsik16@users.noreply.github.com>
2024-03-12 23:22:41 +0530

    some animation

--- project-1/src/App.jsx
+++ project-1/src/App.jsx
@@ -1,19 +1,30 @@
-import { useState } from 'react'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import { gsap } from 'gsap';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    var t1 =gsap.timeline()
+    t1.from('#navbar img,#center h1,#right',{ y: -100,duration:1,delay:1,opacity:0,repeat:3,stagger:0.5});
+    
+  }, []);
 
   return (
     <>
       <div id="main">
     <nav id="navbar">
-      <div id="leftlogo"><img src="/images/brand_logo.png" >
+      <div id="leftlogo"><img src="./images/logoo-removebg-preview.png" >
         </img>
       </div>
       <div id="center">
+        <h1>project</h1>
+        <h1>about</h1>
+        <h1>contact</h1>
         
       </div>
+      <div id='right'><button id='loginbut'>login</button></div>
     </nav>
       <div id="page1">
             <h1>change the course</h1>
