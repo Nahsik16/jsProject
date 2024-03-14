116a057b756ef904a7555746c520aec5e137b3f9
Nahsik16 <118239409+Nahsik16@users.noreply.github.com>
2024-03-14 23:06:06 +0530

    edit 14/03

--- project-1/src/App.jsx
+++ project-1/src/App.jsx
@@ -6,28 +6,31 @@ function App() {
   const [count, setCount] = useState(0);
 
   useEffect(() => {
-    var t1 =gsap.timeline()
-    t1.from('#navbar img,#center h1,#right',{ y: -100,duration:1,delay:1,opacity:0,repeat:3,stagger:0.5});
+    const t1 =gsap.timeline();
     
+   t1.from('#leftlogo img,h3',{ y: -100, duration: 2,delay:0});
+   //t1.to('#page1 h1', { x: 100, duration: 2,delay:1,yoyo:true, stagger: 1.5})
+
   }, []);
 
   return (
     <>
-      <div id="main">
+    <div id="main">
     <nav id="navbar">
       <div id="leftlogo"><img src="./images/logoo-removebg-preview.png" >
         </img>
       </div>
       <div id="center">
-        <h1>project</h1>
-        <h1>about</h1>
-        <h1>contact</h1>
+        <h3>project</h3>
+        <h3>about</h3>      
+        <h3>contact</h3>
         
       </div>
       <div id='right'><button id='loginbut'>login</button></div>
     </nav>
       <div id="page1">
-            <h1>change the course</h1>
+            <h1>change the</h1>
+              <h1>course</h1>
       </div>
       <div id="video-container">
         <div id="play">PLAY</div>
